575fd1712021b2e2b93430ba0bd2e52e1e59af0d
joelsamthomas <joeltom216@gmail.com>
2022-03-26 01:16:50 +0530

    1 file modified

--- src/ParentComponent.jsx
+++ src/ParentComponent.jsx
@@ -8,20 +8,31 @@ export default function ParentComponent() {
   const [age, setAge] = useState(30);
   const [salary, setSalary] = useState(50000);
 
+
+  //commented the actual callback and used useCallBack Hook to avoid unnecessary renders
+  //of all the compoents when age or salary is chnaged
+
+
   /*const IncrementAge = () => {
     setAge(age + 1);
   };*/
+
+  const incrementAge = useCallback(() => {
+    setAge(age + 1)}, [age])
+
+
+
 
   const incrementSal = useCallback(() => {
     setSalary(salary + 10000);
   }, [salary]);
   
-  const incrementAge = useCallback(() => {
-    setAge(age + 1)}, [age])
+
   
   /*const IncrementSalary = () => {
     setSalary(salary + 10000);
   };*/
+
  console.log('Redering ParentComponet');
   return (
     <>
